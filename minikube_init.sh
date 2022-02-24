21b038024aafa9fd2f8b3949a75314f7407ea7ba
EC2 Default User <ec2-user@ip-192-168-0-125.ap-northeast-1.compute.internal>
2022-02-24 00:29:20 +0000

    Added kubectl complition

---
+++ minikube_init.sh
@@ -0,0 +1,15 @@
+alias kubectl='minikube kubectl --'
+alias k='minikube kubectl --'
+alias dps='docker ps'
+
+minikube start
+# minikube stop
+
+eval $(minikube docker-env)
+# eval $(minikube docker-env --unset)
+
+# kubectl proxy --address 0.0.0.0 --accept-hosts '.*' --port 8080
+# Access to https://xxx.amazonaws.com:8080/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/
+
+kubectl completion bash > ./kubectl_comp.sh
+source ./kubectl_comp.sh
